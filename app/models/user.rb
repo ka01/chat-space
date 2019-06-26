ddbaec70affbd67527d4fe2546b022b7568e99f9
ka01 <kaoru.kmt.21@gmail.com>
2019-06-26 22:26:08 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_many :messages
   has_many :group_users
   has_many :groups, through: :group_users
