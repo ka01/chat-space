9db20f9da19475158d60efdecdd9f3a1bfaf8a51
ka01 <kaoru.kmt.21@gmail.com>
2019-06-28 00:55:44 +0900

    Update models/group.rb

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :group_users
-  validates :name, presence: true, uniqueness: true
   has_many :messages
+  validates :name, presence: true, uniqueness: true
 end
