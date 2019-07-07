e9d7fb097d96937763b7c8643243c8aee6c11dab
ka01 <kaoru.kmt.21@gmail.com>
2019-07-07 21:57:43 +0900

    Create and Update message.js
    for ajax

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,47 @@
+$(function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img class="lower-message__image" src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+    <div class="message__talker-info">
+    <div class="talker-name">
+    ${message.user_name}
+    </div>
+    <div class="date">
+    ${message.date}
+    </div>
+    </div>
+    <div class="message-text">
+    <p class="lower-message__content">
+    ${content}
+    </p>
+    </div>
+    ${img}
+    </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $('.form__message').val('');
+    })
+    .fail(function(data){
+      alert('メッセージを入力してください。');
+    })
+    .always(function(data){
+      $('.submit-btn').prop('disabled', false);
+    })
+  })
+})
