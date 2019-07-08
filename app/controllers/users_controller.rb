b01f6db2f965b2016b3eb2e504a083be6dc148e6
ka01 <kaoru.kmt.21@gmail.com>
2019-07-09 04:05:52 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+      respond_to do |format|
+        format.html
+        format.json
+      end
+  end
+
   def edit
   end
 
