7f66f49bd46aa1e95dd4582415c83177fddb0248
ka01 <kaoru.kmt.21@gmail.com>
2019-07-15 01:40:29 +0900

    Update messages.js
    Add Automatic Updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img class="lower-message__image" src= ${ message.image }>` : "";
-    var html = `<div class="message" data-id="${message.id}">
+    var html = `<div class="message" data-message-id="${message.id}">
     <div class="message__talker-info">
     <div class="talker-name">
     ${message.user_name}
@@ -44,4 +44,28 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     })
   })
-})
+  
+  var reloadMessages = function() {
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message').last().data("message-id");
+      $.ajax({
+        url: 'api/messages',
+        type: 'GET',
+        dataType: 'json',
+        data: { id: last_message_id }
+      })
+      .done(function (messages) {
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message);
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function() {
+        alert('自動更新に失敗しました');
+      });
+    }
+  };
+  setInterval(reloadMessages, 5000);
+});
