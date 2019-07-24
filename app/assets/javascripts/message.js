cad77920e78f199df0f842266d9d90ecab17c1d7
ka01 <kaoru.kmt.21@gmail.com>
2019-07-25 00:54:59 +0900

    Update message.js
    Remove turbolinks

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+$(function(){
   function buildHTML(message) {
     var content = message.content ? `${ message.content }` : "";
     var img = message.image ? `<img class="lower-message__image" src= ${ message.image }>` : "";
